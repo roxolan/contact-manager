2ff1a8d85b125ebe06b053c3e67f84bfb9172243
Viktor Kotusenko <roxolanus@gmail.com>
2018-05-08 23:54:10 -0400

    refactoring reducer

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,7 +1,16 @@
+import { ADD_CONTACT } from "../constants/actionTypes";
+
 const initialState = {
   contacts: []
 };
 
-const rootReducer = (state = initialState, action) => state;
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+  case ADD_CONTACT:
+    return { ...state, contacts: [...state.contacts, action.payload] };
+  default:
+    return state;
+  }
+};
 
 export default rootReducer;
