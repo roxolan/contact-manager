cd385287986fc7f3870b0234927cff1e0c118e70
Viktor Kotusenko <roxolanus@gmail.com>
2018-05-09 00:27:02 -0400

    rollback from react-router -> react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './js/store/index';
 import App from './js/components/container/App';
 import style from './main.css';
-import routes from './js/routes';
 
-module.exports = render((
-  <Provider store={store}>
-    {routes}
-  </Provider>
-), document.getElementById("app"));
+render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById("app")
+);
