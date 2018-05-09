ab27bdfb5f43c39d75c8a89e6a7c91b9cb90d81b
Viktor Kotusenko <roxolanus@gmail.com>
2018-05-09 00:38:35 -0400

    Create navigation menu & define routes

--- src/js/components/container/App.js
+++ src/js/components/container/App.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
+import { NavLink, Route } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
 import ContactListContainer from './ContactListContainer';
 import ContactFormContainer from './ContactFormContainer';
 
-const App = () => (
-  <div className="row mt-5">
-    <div className="col-md-4 offset-md-1">
-      <h2>Contacts List :::</h2>
-      <ContactListContainer />
-    </div>
-    <div className="col-md-4 offset-md-1">
-      <h2>-> Add a New Contact</h2>
-      <ContactFormContainer />
-    </div>
-  </div>
-);
+class App extends Component {
+  render() {
+    return (
+      <Container>
+        <div className="row mt-5">
+          <div className="col-md-4 offset-md-1">
+            <h2>Contacts List :::</h2>
+            <ContactListContainer />
+          </div>
+          <div className="col-md-4 offset-md-1">
+            <h2>-> Add a New Contact</h2>
+            <ContactFormContainer />
+          </div>
+        </div>
+      </Container>
+    );
+  }
+}
+
 export default App;
